8e100dd710bad43852fc47b607fd271af217c684
panguk7564 <bongdal7564@gmail.com>
2023-12-19 17:48:51 +0900

    1219
    영속성 설정을 알고 합시다

--- Shop/src/main/java/scripts/Shop/Entity/Order/Oorder.java
+++ Shop/src/main/java/scripts/Shop/Entity/Order/Oorder.java
@@ -19,7 +19,7 @@ public class Oorder {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @ManyToOne(fetch = FetchType.LAZY)
     private Uuser user;
 
     @Builder
